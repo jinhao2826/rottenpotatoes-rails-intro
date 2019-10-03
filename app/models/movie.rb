18d4047536e7d2c4740a6a82d1d155d4555649b5
jinhao2826 <jinhao2826@126.com>
2019-10-02 23:53:30 -0500

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ActiveRecord::Base
-    def self.all_ratings
-        self.all.select(:rating).distinct.pluck(:rating)
-    end
+	def self.ratings
+		Movie.select(:rating).distinct.inject([]){|a,m| a.push m.rating}
+	end
+
 end
